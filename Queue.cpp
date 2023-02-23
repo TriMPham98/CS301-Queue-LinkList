c22875975bf3ca6399ec33a2ad45d8aa663c7424
Tri Pham <70353051+TriMPham98@users.noreply.github.com>
2023-02-23 11:40:38 -0800

    Initial commit

---
+++ Queue.cpp
@@ -0,0 +1,51 @@
+#include "Queue.h"
+
+
+template<class T>
+Queue<T>::Queue(int max)
+{
+
+}
+
+template<class T>
+Queue<T>::Queue()
+{
+
+}
+
+template<class T>
+Queue<T>::~Queue()
+{
+
+}
+
+template<class T>
+void Queue<T>::MakeEmpty()
+{
+
+}
+
+template<class T>
+bool Queue<T>::IsEmpty() const
+{
+    return false;
+}
+
+template<class T>
+bool Queue<T>::IsFull() const
+// Returns true if the queue is full; false otherwise.
+{
+    return false;
+}
+
+template<class T>
+void Queue<T>::Enqueue(T newItem)
+{
+
+}
+
+template<class T>
+T Queue<T>::Dequeue()
+{
+    return T();
+}
