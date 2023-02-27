f0d0082d8f7fa5c75fb66e6418efc69c7afc4caf
Tri <trimpham98@gmail.com>
2023-02-27 00:11:28 -0800

    Add MakeEmpty method

--- Queue.cpp
+++ Queue.cpp
@@ -1,34 +1,34 @@
 #include "Queue.h"
 
 template<class T>
-Queue<T>::Queue()
-{
+Queue<T>::Queue() {
     head = nullptr;
     tail = nullptr;
 }
 
 template<class T>
-Queue<T>::Queue(int max)
-{
+Queue<T>::Queue(int max) {
     head = nullptr;
     tail = nullptr;
 }
 
 template<class T>
-Queue<T>::~Queue()
-{
-
+Queue<T>::~Queue() {
+    MakeEmpty();
 }
 
 template<class T>
-void Queue<T>::MakeEmpty()
-{
-
+void Queue<T>::MakeEmpty() {
+    while (head != nullptr) {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+    }
+    tail = nullptr;
 }
 
 template<class T>
-bool Queue<T>::IsEmpty() const
-{
+bool Queue<T>::IsEmpty() const {
     return false;
 }
 
@@ -40,13 +40,11 @@ bool Queue<T>::IsFull() const
 }
 
 template<class T>
-void Queue<T>::Enqueue(T newItem)
-{
+void Queue<T>::Enqueue(T newItem) {
 
 }
 
 template<class T>
-T Queue<T>::Dequeue()
-{
+T Queue<T>::Dequeue() {
     return T();
 }
